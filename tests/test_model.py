f51a9426ef9a5e1110c0a28540b41d3dcc62cf7c
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2023-10-15 16:05:35 +0100

    Added initial files

---
+++ tests/test_model.py
@@ -0,0 +1,20 @@
+import json
+import pytest
+
+
+from azure_datamodel.model import Model
+
+
+@pytest.mark.usefixtures("model", "modelpath")
+class TestSerializeModel:
+    def test_build_model_success(self, model):
+        assert isinstance(model, Model)
+
+    def test_serialize_success(self, model, modelpath):
+        with open(modelpath, "w") as f:
+            f.write(model.model_dump_json(indent=4))
+        # Load from disk
+        with open(modelpath, "r") as f:
+            lines = f.read()
+            loaded = Model.model_validate_json(lines)
+        assert model
